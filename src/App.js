3e460abb9f571de2425e51cc6f71c3ed4b455364
kimberlyzhu <kimberly.zhu.2000@gmail.com>
2020-07-09 20:52:56 -0700

    made api request with openweather map

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import WeatherCard from "./components/WeatherCard/component";
 import "./App.css";
 
 function App() {
+  const data = async () => {
+    const apiRes = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=dddfe51d9e932708e36c71fd64e15b96"
+    );
+    const resJSON = await apiRes.json();
+    return resJSON;
+  };
+  //.then statement has access to value of promise, then you can do something to value
+  data().then((res) => {
+    console.log("feels like is: " + res.main.feels_like);
+    console.log("temp is: " + res.main.temp);
+  });
+
   return (
     <div className="App">
       <WeatherCard temp={80} condition="Clear" city="Cupertino" country="US" />
